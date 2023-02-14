2943924bc7e736179114b52f1c05f0efa39fe0da
ItayMizrachi <125411823+ItayMizrachi@users.noreply.github.com>
2023-02-15 00:35:35 +0200

    Delete mongoConnect.js

--- db/mongoConnect.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-main().catch(err => console.log(err));
-async function main() {
-    mongoose.set('strictQuery', false);
-    await mongoose.connect('mongodb://127.0.0.1:27017/fullstack23');
-    console.log("Mongo Connected! ")
-}
